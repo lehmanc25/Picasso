f397be857276674729eaf4591524e06483d0265d
Nabil <nyoussef@mail.wlu.edu>
2023-11-28 22:36:11 -0500

    wrote the ceil unary operator

---
+++ src/picasso/parser/language/expressions/Ceil.java
@@ -0,0 +1,43 @@
+package picasso.parser.language.expressions;
+
+import picasso.parser.language.ExpressionTreeNode;
+
+/**
+ * Represents the Ceil function in the Picasso language.
+ * 
+ * @author Nabil Youssef
+ * 
+ */
+public class Ceil extends UnaryFunction {
+
+	/**
+	 * Create a floor expression that takes as a parameter the given expression
+	 * 
+	 * @param param the expression to floor
+	 */
+	public Ceil(ExpressionTreeNode param) {
+		super(param);
+	}
+
+	/**
+	 * Evaluates this expression at the given x,y point by evaluating the floor of
+	 * the function's parameter.
+	 * 
+	 * @return the color from evaluating the floor of the expression's parameter
+	 */
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor result = param.evaluate(x, y);
+		double red = Math.ceil(result.getRed());
+		double green = Math.ceil(result.getGreen());
+		double blue = Math.ceil(result.getBlue());
+
+		return new RGBColor(red, green, blue);
+	}
+	
+	@Override
+    public String toString() {
+        return "ceil(" + this.param + ")";
+    }
+
+}
