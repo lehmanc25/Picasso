37c70f355d6f48e51c47b6c3b2300d595587635b
Ford Scott <scottf25@mail.wlu.edu>
2023-11-29 11:37:33 -0500

    Add AbsFunction and AbsToken for absolute value support
    - Implemented AbsFunction class extending UnaryFunction to handle the
    calculation of absolute values of expressions in the Picasso project.
    - Added AbsToken in the lexer to recognize 'abs' keyword in expressions.
    - These additions allow users to apply the absolute value operation on
    expressions, enhancing the mathematical capabilities of the application.

---
+++ src/picasso/parser/language/expressions/AbsFunction.java
@@ -0,0 +1,30 @@
+public class AbsFunction extends UnaryFunction {
+
+    /**
+     * Constructor for AbsFunction.
+     * @param param The expression node that this function will be applied to.
+     */
+    public AbsFunction(ExpressionTreeNode param) {
+        super(param);
+    }
+
+    /**
+     * Evaluates the absolute value function on the parameter.
+     * @param x The x-coordinate at which to evaluate the function.
+     * @param y The y-coordinate at which to evaluate the function.
+     * @return The result of applying the absolute value function to the parameter.
+     */
+    @Override
+    public RGBColor evaluate(double x, double y) {
+        // First, evaluate the parameter (expression node) at the given x and y
+        RGBColor color = param.evaluate(x, y);
+
+        // Apply the absolute value function to each component of the RGBColor
+        double red = Math.abs(color.getRed());
+        double green = Math.abs(color.getGreen());
+        double blue = Math.abs(color.getBlue());
+
+        // Return the new RGBColor with the absolute values
+        return new RGBColor(red, green, blue);
+    }
+}
