07f9a06c0f0563178d01cd816f49ca3690e5fa47
desire04 <asinyad25@mail.wlu.edu>
2023-11-28 22:23:54 -0500

    Created the token for the cos function.

---
+++ src/picasso/parser/tokens/functions/CosToken.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the cosine function token
+ */
+public class CosToken extends FunctionToken {
+
+	public CosToken() {
+		super("Cosine function token");
+	}
+
+}
