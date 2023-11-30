c50a1b411c1c814c1af88002a1aa3bef3b8dc9ee
Nabil <nyoussef@mail.wlu.edu>
2023-11-30 17:05:05 -0500

    added ceil test

--- src/tests/TokenizerTest.java
+++ src/tests/TokenizerTest.java
@@ -90,6 +90,15 @@ public class TokenizerTest {
 		assertEquals(new LeftParenToken(), tokens.get(1));
 		assertEquals(new IdentifierToken("x"), tokens.get(2));
 		assertEquals(new RightParenToken(), tokens.get(3));
+		
+		
+		String expression3 = "ceil(x)";
+		tokens = tokenizer.parseTokens(expression3);
+		assertEquals(new CeilToken(), tokens.get(0));
+		assertEquals(new LeftParenToken(), tokens.get(1));
+		assertEquals(new IdentifierToken("x"), tokens.get(2));
+		assertEquals(new RightParenToken(), tokens.get(3));
+		
 	}
 
 	@Test
