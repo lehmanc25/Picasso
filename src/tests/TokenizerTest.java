99afe147294e52d00b3543f8356dba4ab3f3976c
desire04 <asinyad25@mail.wlu.edu>
2023-11-30 01:56:40 -0500

    Added tokenizer tests for addition expressions.

--- src/tests/TokenizerTest.java
+++ src/tests/TokenizerTest.java
@@ -13,6 +13,7 @@ import picasso.parser.Tokenizer;
 import picasso.parser.tokens.*;
 import picasso.parser.tokens.chars.*;
 import picasso.parser.tokens.functions.*;
+import picasso.parser.tokens.operations.PlusToken;
 
 /**
  * Tests that the tokenizer tokens as expected. 
@@ -110,7 +111,16 @@ public class TokenizerTest {
 		tokens = tokenizer.parseTokens(expression);
 		// TODO: Check the tokens...
 	}
-
-	// TODO: Test arithmetic (rather than function-based) expressions ...
+	@Test
+	public void testTokenizeArithmeticExpressions() {
+		String expression1 = "x+y";
+		tokens = tokenizer.parseTokens(expression1);
+		assertEquals(new IdentifierToken("x"), tokens.get(0));
+		assertEquals(new PlusToken(), tokens.get(1));
+		assertEquals(new IdentifierToken("y"), tokens.get(2));
+		
+		
+		
+	}
 
 }
