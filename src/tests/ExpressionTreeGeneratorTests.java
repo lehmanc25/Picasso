9a897c94849b5645a5f9dfea60e754e6200e3054
Nabil <nyoussef@mail.wlu.edu>
2023-11-30 16:54:22 -0500

    added ceil tests

--- src/tests/ExpressionTreeGeneratorTests.java
+++ src/tests/ExpressionTreeGeneratorTests.java
@@ -90,5 +90,14 @@ public class ExpressionTreeGeneratorTests {
 		e = parser.makeExpression("floor( x + y )");
 		assertEquals(new Floor(new Addition(new X(), new Y())), e);
 	}
+	
+	@Test
+	public void ceilFunctionTests() {
+		ExpressionTreeNode e = parser.makeExpression("ceil( x )");
+		assertEquals(new Ceil(new X()), e);
+
+		e = parser.makeExpression("ceil( x + y )");
+		assertEquals(new Ceil(new Addition(new X(), new Y())), e);
+	}
 
 }
