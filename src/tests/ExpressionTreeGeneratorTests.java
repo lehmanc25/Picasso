1439ebbd8075e6ca69cd7aed8cc7ed861312dd61
desire04 <asinyad25@mail.wlu.edu>
2023-12-05 11:14:41 -0500

    Added tests for the multiplication operator as appropriate.

--- src/tests/ExpressionTreeGeneratorTests.java
+++ src/tests/ExpressionTreeGeneratorTests.java
@@ -59,6 +59,22 @@ public class ExpressionTreeGeneratorTests {
 		e = parser.makeExpression("x + y + [ -.51, 0, 1]");
 		assertEquals(new Addition(new Addition(new X(), new Y()), new RGBColor(-.51, 0, 1)), e);
 	}
+	
+	@Test
+	public void multiplicationExpressionTests() {
+		ExpressionTreeNode e = parser.makeExpression("x * y");
+		assertEquals(new Multiplication(new X(), new Y()), e);
+
+		// no spaces!
+		e = parser.makeExpression("x*y");
+		assertEquals(new Multiplication(new X(), new Y()), e);
+
+		e = parser.makeExpression("[1,.3,-1] * y");
+		assertEquals(new Multiplication(new RGBColor(1, .3, -1), new Y()), e);
+
+		e = parser.makeExpression("x * y * [ -.51, 0, 1]");
+		assertEquals(new Multiplication(new Multiplication(new X(), new Y()), new RGBColor(-.51, 0, 1)), e);
+	}
 
 	@Test
 	public void parenthesesExpressionTests() {
