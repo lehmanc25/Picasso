ff662b3549e7ed860062d883d62146cd1909c4f4
Connor Lehman <lehmanc25@mail.wlu.edu>
2023-12-06 17:01:07 -0500

    Refactored createExpression, removed render dup code and overloaded the execute method

--- src/picasso/view/commands/Evaluator.java
+++ src/picasso/view/commands/Evaluator.java
@@ -2,6 +2,7 @@ package picasso.view.commands;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.io.File; 
 
 import javax.swing.JTextField;
 
@@ -13,35 +14,62 @@ import picasso.util.Command;
 /**
  * Evaluate an expression for each pixel in a image.
  * 
- * @author Robert C Duvall
+ * @author Robert C Duvall 
  * @author Sara Sprenkle
+ * @author Hotshots
  */
 public class Evaluator implements Command<Pixmap> {
 	public static final double DOMAIN_MIN = -1;
 	public static final double DOMAIN_MAX = 1;
 
 
-	JTextField textfield;
+	protected JTextField textfield;
+	protected File file;
 	
+	//GUI Evaluator
 	public Evaluator(JTextField tf) {
 		this.textfield = tf;
 	}
+	
+	
 	/**
-	 * Evaluate an expression for each point in the image.
+	 * Renders the expression for each pixel on Pixmap.
+	 * @param target
+	 * @param expression tree node 
 	 */
-	public void execute(Pixmap target) {
-		// create the expression to evaluate just once
-		ExpressionTreeNode expr = createExpression();
-		// evaluate it for each pixel
+	public void render(Pixmap target, ExpressionTreeNode expr) {
+		// evaluate ExpressionTreeNode for each pixel
 		Dimension size = target.getSize();
 		for (int imageY = 0; imageY < size.height; imageY++) {
 			double evalY = imageToDomainScale(imageY, size.height);
 			for (int imageX = 0; imageX < size.width; imageX++) {
-				double evalX = imageToDomainScale(imageX, size.width);
-				Color pixelColor = expr.evaluate(evalX, evalY).toJavaColor();
-				target.setColor(imageX, imageY, pixelColor);
+				double evalX = imageToDomainScale(imageX, size.width);		
+						Color pixelColor = expr.evaluate(evalX, evalY).toJavaColor();
+						target.setColor(imageX, imageY, pixelColor);
 			}
-		}
+		}		
+	}
+	/**
+	 * Evaluate an expression from GUI input.
+	 * @param target
+	 * @param expression
+	 */
+	public void execute(Pixmap target) {
+		// create the expression to evaluate
+		ExpressionTreeNode expression = createExpression(textfield.getText());
+		//render the expression
+		render(target, expression);	
+	}
+	/**
+	 * Evaluate an expression from a file.
+	 * @param target
+	 * @param expression
+	 */
+	public void execute(Pixmap target, String fileString) {
+		// create the expression to evaluate
+		ExpressionTreeNode expression = createExpression(fileString);
+		//render the expression
+		render(target, expression);
 	}
 
 	/**
@@ -53,18 +81,16 @@ public class Evaluator implements Command<Pixmap> {
 	}
 
 	/**
-	 * 
 	 * A place holder for a more interesting way to build the expression.
 	 */
-	private ExpressionTreeNode createExpression() {
+	private ExpressionTreeNode createExpression(String input) {
 		// Note, when you're testing, you can use the ExpressionTreeGenerator to
 		// generate expression trees from strings, or you can create expression
 		// objects directly (as in the commented statement below).
 
 		// String test = "floor(y)";
 		// String test = "x + y";
-		
-		String input = textfield.getText();
+
 		ExpressionTreeGenerator expTreeGen = new ExpressionTreeGenerator();
 		return expTreeGen.makeExpression(input);
 		
