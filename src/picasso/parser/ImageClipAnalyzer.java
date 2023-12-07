6af336634f3d7f80879f5fd695c9b45b91208d3e
Ford Scott <scottf25@mail.wlu.edu>
2023-12-07 11:43:51 -0500

    Updating ImageClip

--- src/picasso/parser/ImageClipAnalyzer.java
+++ src/picasso/parser/ImageClipAnalyzer.java
@@ -2,8 +2,9 @@ package picasso.parser;
 
 import java.util.Stack;
 import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Image;
 import picasso.parser.language.expressions.ImageClip;
-import picasso.parser.tokens.ImageToken;
+import picasso.parser.tokens.QuoteToken; // Import QuoteToken class
 import picasso.parser.tokens.Token;
 
 /**
@@ -13,27 +14,27 @@ import picasso.parser.tokens.Token;
  * @author Ford Scott
  */
 public class ImageClipAnalyzer implements SemanticAnalyzerInterface {
-	
-	/**
-	 * Generate an expression tree for the ImageClip function.
-	 * 
-	 * @param tokens A stack of tokens to process.
-	 * @return An ExpressionTreeNode representing the parsed ImageClip expression.
-	 * @throws ParseException If no Image Token is found in the stack.
-	 */
-	@Override
-	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) throws ParseException {
-		tokens.pop(); // Remove the ImageClipToken.
-		
-		ExpressionTreeNode paramx = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
-		ExpressionTreeNode paramy = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
-		
-		if (tokens.peek() instanceof ImageToken) {
-			ImageAnalyzer analyzer = new ImageAnalyzer();
-			ExpressionTreeNode newImage = analyzer.generateExpressionTree(tokens);
-			return new ImageClip((Image) newImage, paramx, paramy);
-		} else {
-			throw new ParseException("No Image Token Found.");
-		}
-	}
+    
+    /**
+     * Generate an expression tree for the ImageClip function.
+     * 
+     * @param tokens A stack of tokens to process.
+     * @return An ExpressionTreeNode representing the parsed ImageClip expression.
+     * @throws ParseException If no Image Token is found in the stack.
+     */
+    @Override
+    public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) throws ParseException {
+        tokens.pop(); // Remove the ImageClipToken.
+        
+        ExpressionTreeNode paramx = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+        ExpressionTreeNode paramy = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
+        
+        if (tokens.peek() instanceof QuoteToken) {
+            Image img = new Image((QuoteToken) tokens.pop());
+            return new ImageClip(img, paramx, paramy);
+        } else {
+            throw new ParseException("No Image Token (QuoteToken) Found.");
+        }
+    }
 }
+
--- src/picasso/parser/language/expressions/ImageClip.java
+++ src/picasso/parser/language/expressions/ImageClip.java
@@ -9,37 +9,38 @@ import picasso.parser.language.ExpressionTreeNode;
  * @author Ford Scott
  */
 public class ImageClip extends ExpressionTreeNode {
-	
-	public static Image img;
-	private ExpressionTreeNode paramx;
-	private ExpressionTreeNode paramy;
-	
-	/**
-	 * Constructs an ImageClip expression with the specified image and coordinate expressions.
-	 * 
-	 * @param img    The image to clip.
-	 * @param paramx The x-coordinate expression.
-	 * @param paramy The y-coordinate expression.
-	 */
-	public ImageClip(Image img, ExpressionTreeNode paramx, ExpressionTreeNode paramy) {
-		this.paramx = paramx;
-		this.paramy = paramy;
-		this.img = img;
-	}
-	
-	/**
-	 * Evaluates the ImageClip expression by evaluating the x and y coordinate expressions
-	 * and applying the resulting color to the image.
-	 * 
-	 * @param x The x-coordinate.
-	 * @param y The y-coordinate.
-	 * @return The resulting RGB color after applying the clipping operation.
-	 */
-	public RGBColor evaluate(double x, double y) {
-		double xexpr = Clamp.clamp((paramx.evaluate(x, y)).getRed());
-		double yexpr = Clamp.clamp((paramy.evaluate(x, y)).getRed());
-		
-		RGBColor imgColor = img.evaluate(xexpr, yexpr);
-		return imgColor;
-	}
+    
+    public static Image img;
+    private ExpressionTreeNode paramx;
+    private ExpressionTreeNode paramy;
+    
+    /**
+     * Constructs an ImageClip expression with the specified image and coordinate expressions.
+     * 
+     * @param img    The image to clip.
+     * @param paramx The x-coordinate expression.
+     * @param paramy The y-coordinate expression.
+     */
+    public ImageClip(Image img, ExpressionTreeNode paramx, ExpressionTreeNode paramy) {
+        this.paramx = paramx;
+        this.paramy = paramy;
+        ImageClip.img = img;
+    }
+    
+    /**
+     * Evaluates the ImageClip expression by evaluating the x and y coordinate expressions
+     * and applying the resulting color to the image.
+     * 
+     * @param x The x-coordinate.
+     * @param y The y-coordinate.
+     * @return The resulting RGB color after applying the clipping operation.
+     */
+    public RGBColor evaluate(double x, double y) {
+        double xexpr = Clamp.clamp((paramx.evaluate(x, y)).getRed());
+        double yexpr = Clamp.clamp((paramy.evaluate(x, y)).getRed());
+        
+        RGBColor imgColor = img.evaluate(xexpr, yexpr);
+        return imgColor;
+    }
 }
+
