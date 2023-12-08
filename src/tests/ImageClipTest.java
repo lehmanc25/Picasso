75b82c3057e544789d397a854050d689f7249f5b
Ford Scott <scottf25@mail.wlu.edu>
2023-12-08 11:14:14 -0500

    Update tests

--- src/tests/ImageClipTest.java
+++ src/tests/ImageClipTest.java
@@ -1,34 +1,70 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
-import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.Image;
+import org.junit.jupiter.api.Test;
 import picasso.parser.language.expressions.ImageClip;
-import picasso.parser.language.expressions.*;
-import picasso.parser.tokens.QuoteToken;
+import picasso.parser.tokens.functions.ImageClipToken;
 import picasso.parser.language.expressions.RGBColor;
+import picasso.parser.language.expressions.X;
+import picasso.parser.language.expressions.Y;
+import picasso.parser.language.expressions.Image;
+import picasso.parser.tokens.QuoteToken;
+import picasso.parser.language.ExpressionTreeNode;
 
 public class ImageClipTest {
+	
+	@Test
+	public void TestImageClipTokenDescription() {
+		ImageClipToken token = new ImageClipToken();
+		assertEquals(token.toString(), "ImageClip Function Token");
+	}
+	
+	@Test
+	public void testImageClipTokenFunction() {
+		ImageClipToken token = new ImageClipToken();
+		assertTrue(token.isFunction()== true);
+	}
+	
+	@Test
+	public void testImageClipTokenNotConstant() {
+		ImageClipToken token = new ImageClipToken();
+		assertTrue(token.isConstant()== false);
+	}
+	
+	@Test
+	public void testImageClipEvaluation() {
+	    QuoteToken filenameToken = new QuoteToken("\"vortex.jpg\""); // Correctly create QuoteToken
+	    Image image = new Image(filenameToken); // Create Image instance
 
-    @Test
-    public void testImageClipFunctionality() {
-        QuoteToken filenameToken = new QuoteToken("vortex.jpg");
-        Image image = new Image(filenameToken);
+	    // Assuming you have constant expression classes X and Y
+	    ExpressionTreeNode xExpr = new X(); // Replace with your actual expression implementation
+	    ExpressionTreeNode yExpr = new Y(); // Replace with your actual expression implementation
 
-        // Example x and y coordinate expressions (create these expressions as needed)
-        ExpressionTreeNode xExpr = new Constant(0.5); // Assuming 0.5 as x coordinate
-        ExpressionTreeNode yExpr = new Constant(-0.5); // Assuming -0.5 as y coordinate
-        
-        ImageClip imageClip = new ImageClip(image, xExpr, yExpr);
+	    ImageClip myTree = new ImageClip(image, xExpr, yExpr);
 
-        // Test the evaluate method of ImageClip
-        RGBColor color = imageClip.evaluate(0.5, 0.5); // Example coordinates
+	    // Test the evaluation method with actual expected colors
+	    // These colors should be determined based on the content of "vortex.jpg"
+	    // Example:
+	    RGBColor expectedColor = new RGBColor(-6, -6, -6); // Replace with actual expected color
+	    RGBColor actualColor = myTree.evaluate(-1, -1);
+	    assertEquals(expectedColor, actualColor, "Color should match expected value");
 
-        assertNotNull(color, "Color should not be null");
-        // Additional assertions based on expected color values
-    }
-
-    // Additional test cases for different expressions, edge cases, etc.
+	    // Additional tests can be added here
+		double[] tests = {7, -10, 100, -.5};
+		
+		for (double testVal : tests) {
+			if(testVal>1) {
+				double clampOfTestVal = 1;
+				assertEquals(new RGBColor(clampOfTestVal, clampOfTestVal, clampOfTestVal), myTree.evaluate(1, -1));
+			}
+			if(testVal<-1) {
+				double clampOfTestVal = -1;
+				assertEquals(new RGBColor(clampOfTestVal, clampOfTestVal, clampOfTestVal), myTree.evaluate(-1, -1));
+			}
+			else {
+				double clampOfTestVal = testVal;
+				assertEquals(new RGBColor(clampOfTestVal, clampOfTestVal, clampOfTestVal), myTree.evaluate(testVal, -1));
+			}
+		}
+	}
 }
