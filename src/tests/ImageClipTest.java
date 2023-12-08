f21f5333c3153e8643335b44b5abac7036dbb139
Ford Scott <scottf25@mail.wlu.edu>
2023-12-07 20:52:48 -0500

    TESTS

---
+++ src/tests/ImageClipTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Image;
+import picasso.parser.language.expressions.ImageClip;
+import picasso.parser.language.expressions.*;
+import picasso.parser.tokens.QuoteToken;
+import picasso.parser.language.expressions.RGBColor;
+
+public class ImageClipTest {
+
+    @Test
+    public void testImageClipFunctionality() {
+        QuoteToken filenameToken = new QuoteToken("\"testImage.jpg\"");
+        Image image = new Image(filenameToken);
+
+        // Example x and y coordinate expressions (create these expressions as needed)
+        ExpressionTreeNode xExpr = new Constant(0.5); // Assuming 0.5 as x coordinate
+        ExpressionTreeNode yExpr = new Constant(-0.5); // Assuming -0.5 as y coordinate
+        
+        ImageClip imageClip = new ImageClip(image, xExpr, yExpr);
+
+        // Test the evaluate method of ImageClip
+        RGBColor color = imageClip.evaluate(0.5, 0.5); // Example coordinates
+
+        assertNotNull(color, "Color should not be null");
+        // Additional assertions based on expected color values
+    }
+
+    // Additional test cases for different expressions, edge cases, etc.
+}
