3ad800f7ec815831e693107f0ae4c23630542d40
desire04 <asinyad25@mail.wlu.edu>
2023-12-09 14:32:11 -0500

    Created the token, analyzer, and expression tree node for the sin function.

---
+++ src/picasso/parser/SinAnalyzer.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Sine;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the sine function
+ * 
+ * @author Desire Asinya
+ */
+public class SinAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop();
+		
+		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+				
+		return new Sine(paramETN);
+	}
+
+}
