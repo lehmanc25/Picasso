8e1c9a24eb9490850a5bb845eac5b70d0d04090d
desire04 <asinyad25@mail.wlu.edu>
2023-12-09 14:31:05 -0500

    Created the token, analyzer, and expression tree node of the atan function.

---
+++ src/picasso/parser/AtanAnalyzer.java
@@ -0,0 +1,22 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Atan;
+import picasso.parser.language.expressions.Sine;
+import picasso.parser.tokens.Token;
+
+public class AtanAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop();
+		
+		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+				
+		return new Atan(paramETN);
+	}
+
+}
