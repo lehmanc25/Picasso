a2e4ce617973297755dfb2bedd36fe7bfe256992
desire04 <asinyad25@mail.wlu.edu>
2023-12-09 14:31:38 -0500

    Created the token, analyzer, and expression tree node for the exp token.

---
+++ src/picasso/parser/ExpAnalyzer.java
@@ -0,0 +1,23 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Exp;
+import picasso.parser.language.expressions.Sine;
+import picasso.parser.tokens.Token;
+
+public class ExpAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop();
+		
+		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+				
+		return new Exp(paramETN);
+
+	}
+
+}
---
+++ src/picasso/parser/tokens/functions/ExpToken.java
@@ -0,0 +1,14 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the exp function token
+ * 
+ * @author Desire Asinya
+ */
+public class ExpToken extends FunctionToken {
+
+	public ExpToken() {
+		super("Exp function token");
+	}
+
+}
