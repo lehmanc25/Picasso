a8b5ec4cc3429f85dd378154f4d7813c5263447d
desire04 <asinyad25@mail.wlu.edu>
2023-12-10 02:57:22 -0500

    Created the token, expression tree node, and analyzer for the wrap function.

---
+++ src/picasso/parser/tokens/functions/WrapToken.java
@@ -0,0 +1,18 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the wrap function token.
+ * This class extends FunctionToken and represents the wrap function.
+ * 
+ * @author Desire Asinya
+ */
+public class WrapToken extends FunctionToken {
+
+	/**
+	 * Constructs a WrapToken representing the wrap function.
+	 */
+	public WrapToken() {
+		super("Wrap Function Token");
+	}
+
+}
