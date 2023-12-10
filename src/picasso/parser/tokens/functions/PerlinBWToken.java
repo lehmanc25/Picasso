09486878b2b56692b5567af5fbf509c4a0ba8344
desire04 <asinyad25@mail.wlu.edu>
2023-12-10 02:55:44 -0500

    Created the token, analyzer, and expression tree node for the perlinBW function.

---
+++ src/picasso/parser/tokens/functions/PerlinBWToken.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the perlinBW function token in the Picasso language.
+ * @author Desire Asinya
+ */
+public class PerlinBWToken extends FunctionToken {
+
+	public PerlinBWToken() {
+		super("PerlinBW function token");
+	}
+
+}
